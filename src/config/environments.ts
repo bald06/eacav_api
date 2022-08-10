ca12a1cb90779c74095be1a65a6e9c609f59f6bf
Bryan <bryan.lepe10@gmail.com>
2022-08-10 12:18:38 -0500

    fix-merge

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -3,7 +3,7 @@ export const environments = {
   TYPEORM_HOST: 'localhost',
   TYPEORM_PORT_DB: 5432,
   TYPEORM_USERNAME: 'postgres',
-  TYPEORM_PASSWORD: 'postgres',
+  TYPEORM_PASSWORD: 'Uncharted3',
   TYPEORM_DATABASE: 'ibaco',
   TYPEORM_SYNCHRONIZE: false,
   TYPEORM_AUTOLOAD_ENTITIES: true,
